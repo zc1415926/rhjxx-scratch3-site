3f7adeb407d4acc6419c939e5fe8f29ed4a4a5f2
ZhangChuan <zc1415926@126.com>
2019-12-30 16:45:43 +0800

    add GradeClassManager

---
+++ src/containers/GradeClassManager/GradeClassManager.jsx
@@ -0,0 +1,23 @@
+import react from 'react';
+import {connect} from 'react-redux';
+
+class GradeClassManager extends react.Component{
+    constructor(props){
+        super(props);
+        
+    }
+}
+
+const mapStateToProps = state =>{
+    return{
+
+    }
+}
+
+const mapDispatchToProps = dispatch =>{
+    return{
+
+    }
+}
+
+export default connect()(GradeClassManager);
