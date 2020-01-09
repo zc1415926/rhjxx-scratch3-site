6e10e0fd17568a734bcf48037d2c5de3bc1bff47
ZhangChuan <zc1415926@126.com>
2020-01-09 22:38:10 +0800

    为制作左侧导航栏做准备

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 
 //import Counter from './containers/Counter/Counter';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import MainGUI from './containers/MainGUI';
 import GradesClassesManager from './containers/GradeClassManager/GradeClassManager';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       {/* <Counter /> */}
-       <GradesClassesManager />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+        {/* <Counter /> */}
+        <MainGUI />
+        <GradesClassesManager />
+        </div>
+      </BrowserRouter>
     );
   }
 }
